4491474ef2dd226b06925b63552c219b4b651919
YeongTaek <yeongtaek2109@gmail.com>
2023-05-15 14:27:13 +0900

    스크롤 천천히

--- main.js
+++ main.js
@@ -29,6 +29,13 @@ homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
 
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 // Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', () => {
