6c20154328d2cc428b4a2e161dbbf18241d08032
YeongTaek <yeongtaek2109@gmail.com>
2023-05-18 21:26:40 +0900

    navbar, footer 구성

--- portfolio_react/src/App.js
+++ portfolio_react/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
