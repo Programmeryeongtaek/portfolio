94f2caed817b6de072c01d155f8f2ddc8c5dea42
YeongTaek <yeongtaek2109@gmail.com>
2023-05-19 11:35:39 +0900

    홈페이지 구성 골격

--- portfolio_react/src/App.js
+++ portfolio_react/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
+import Works from './pages/Works';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import './output.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-2xl">
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/works" element={<Works />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ portfolio_react/src/components/Attitude.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Attitude() {
+  return (
+    <div>Attitude</div>
+  )
+}
