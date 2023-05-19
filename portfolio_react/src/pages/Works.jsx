7b73c3921ed4959db9c8cea89389cad7b2be78df
YeongTaek <yeongtaek2109@gmail.com>
2023-05-20 00:38:56 +0900

    page-works _  project완료

--- portfolio_react/src/pages/Works.jsx
+++ portfolio_react/src/pages/Works.jsx
@@ -2,6 +2,48 @@ import React from 'react'
 
 export default function Works() {
   return (
-    <div>Works</div>
+    <section className='section'>
+      <div id='works' className='section__container'>
+        <h1>프로젝트</h1>
+        <h3>공부목록</h3>
+        <h4>수강목록</h4>
+        <div className='work__categories'>
+          <button className='category__btn' data-filter="">All<span className='category__count'></span></button>
+          <button className='category__btn' data-filter="">Vanilla JS<span className='category__count'></span></button>
+          <button className='category__btn' data-filter="">React<span className='category__count'></span></button>
+          <button className='category__btn' data-filter="">Project<span className='category__count'></span></button>
+        </div>
+        <div className='work__projects'>
+          <a href="" className='project' data-type=''>
+            <img src="" alt="프로젝트1 사진" className='project__img' />
+            <div className='project__description'>
+              <h3>프로젝트1 제목</h3>
+              <span>사용 기술</span>
+            </div>
+          </a>
+          <a href="" className='project' data-type=''>
+            <img src="" alt="프로젝트1 사진" className='project__img' />
+            <div className='project__description'>
+              <h3>프로젝트1 제목</h3>
+              <span>사용 기술</span>
+            </div>
+          </a>
+          <a href="" className='project' data-type=''>
+            <img src="" alt="프로젝트1 사진" className='project__img' />
+            <div className='project__description'>
+              <h3>프로젝트1 제목</h3>
+              <span>사용 기술</span>
+            </div>
+          </a>
+          <a href="" className='project' data-type=''>
+            <img src="" alt="프로젝트1 사진" className='project__img' />
+            <div className='project__description'>
+              <h3>프로젝트1 제목</h3>
+              <span>사용 기술</span>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
   )
 }
