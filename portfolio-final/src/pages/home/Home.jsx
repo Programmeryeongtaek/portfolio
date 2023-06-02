56d011de450ecc6f761a1cd22a96885a0d86b88f
YeongTaek <yeongtaek2109@gmail.com>
2023-06-02 11:19:55 +0900

    Home 화면 구성

---
+++ portfolio-final/src/pages/home/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './home.css';
+import Profile from '../../assets/소개.jpg';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+
+const Home = () => {
+  return (
+    <section className="home sction grid">
+      <img src={Profile} alt="" className="home__img" />
+
+      <div className="home__content">
+        <div className="home__data">
+          <h1 className="home__title">
+            <span>학습하는 청년, 오영택</span> 프론트엔드 개발자
+          </h1>
+
+          <p className="home__description">
+            유능한 프론트엔드 개발자로서 자리잡기 위해 끊임없이 배우고 질문하고 정리하고 있습니다.
+          </p>
+
+          <Link to='/about' className='button'>
+            <span className="button__icon">
+              <FaArrowRight />
+            </span>
+          </Link>        
+        </div>
+      </div>
+
+      <div className="color__block"></div>
+    </section>
+  );
+};
+
+export default Home;
