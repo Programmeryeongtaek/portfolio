8f9647feb373d94851e6891037391e77043f1023
YeongTaek <yeongtaek2109@gmail.com>
2023-06-02 11:04:56 +0900

    page 구성

--- portfolio-final/src/App.jsx
+++ portfolio-final/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './components/nav/Nav';
+import ThemeMode from './components/themeMode/ThemeMode';
+import Themes from './components/themes/Themes';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Work from './pages/work/Work';
+import Contact from './pages/contact/Contact';
 
 function App() {
   return (
     <BrowserRouter>
-      
+      <Nav />
+      <ThemeMode />
+      <Themes />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='work' element={<Work />} />
+        <Route path='contact' element={<Contact />} />
+      </Routes>
     </BrowserRouter>
   );
 };
