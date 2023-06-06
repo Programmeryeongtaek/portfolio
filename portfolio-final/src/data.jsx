f3e327784dee54ffff31f7b8897db46960840078
YeongTaek <yeongtaek2109@gmail.com>
2023-06-06 23:18:51 +0900

    data - poersonalInfo, Resume, Skills

---
+++ portfolio-final/src/data.jsx
@@ -0,0 +1,164 @@
+import {
+  FaBriefcase,
+  FaGraduationCap,
+} from 'react-icons/fa';
+
+export const personalInfo = [
+  {
+    id: 1,
+    title: '이름 : ',
+    description: '오 영택',
+  },
+  {
+    id: 2,
+    title: '나이 : ',
+    description: '32',
+  },
+  {
+    id: 3,
+    title: '학력 : ',
+    description: '고졸검정고시',
+  },
+  {
+    id: 4,
+    title: '주소 : ',
+    description: '서울 용산구 보광동',
+  },
+  {
+    id: 5,
+    title: '휴대폰 : ',
+    description: '010-4135-9209',
+  },
+  {
+    id: 6,
+    title: '이메일 : ',
+    description: 'young-taek@naver.com',
+  },
+  {
+    id: 7,
+    title: '키워드 : ',
+    description: '학습, 열정, 질문, 철학',
+  },
+  {
+    id: 8,
+    title: '키워드 : ',
+    description: '독서, 자전거라이딩, 콘텐츠 제작',
+  },
+];
+
+export const resume = [
+  {
+    id: 1,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '기간',
+    title: '제목',
+    desc: '활동설명',
+  },
+  {
+    id: 2,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '기간',
+    title: '제목',
+    desc: '활동설명',
+  },
+  {
+    id: 3,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '기간',
+    title: '제목',
+    desc: '활동설명',
+  },
+  {
+    id: 4,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '기간',
+    title: '제목',
+    desc: '활동설명',
+  },
+  {
+    id: 5,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '기간',
+    title: '제목',
+    desc: '활동설명',
+  },
+  {
+    id: 6,
+    category: 'learn',
+    icon: <FaGraduationCap />,
+    year: '기간',
+    title: '제목',
+    desc: '활동설명',
+  },
+  {
+    id: 7,
+    category: 'learn',
+    icon: <FaGraduationCap />,
+    year: '기간',
+    title: '제목',
+    desc: '활동설명',
+  },
+  {
+    id: 8,
+    category: 'learn',
+    icon: <FaGraduationCap />,
+    year: '기간',
+    title: '제목',
+    desc: '활동설명',
+  },
+  {
+    id: 9,
+    category: 'learn',
+    icon: <FaGraduationCap />,
+    year: '기간',
+    title: '제목',
+    desc: '활동설명',
+  },
+  {
+    id: 10,
+    category: 'learn',
+    icon: <FaGraduationCap />,
+    year: '기간',
+    title: '제목',
+    desc: '활동설명',
+  },
+];
+
+export const skills = [
+  {
+    id: 1,
+    title: 'HTML',
+    percentage: '50',
+    content :'asdasd',
+  },
+  {
+    id: 2,
+    title: 'CSS',
+    percentage: '50',
+    content :'asdasd',
+  },
+  {
+    id: 3,
+    title: 'JavaScript',
+    percentage: '50',
+    content :'asdasd',
+  },
+  {
+    id: 4,
+    title: 'TypeScript',
+    percentage: '50',
+    standard: '',
+    content :'asdasd',
+  },
+  {
+    id: 5,
+    title: 'React',
+    percentage: '50',
+    content :'asdasd',
+  },
+];
