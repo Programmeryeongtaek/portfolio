a665809f5fb4f46f5a4c539081526291631ee9e9
YeongTaek <yeongtaek2109@gmail.com>
2023-06-07 10:01:03 +0900

    ThemeMode 구상 및 CSS

--- portfolio-final/src/components/themeMode/ThemeMode.jsx
+++ portfolio-final/src/components/themeMode/ThemeMode.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './themeMode.css';
 import { themeMode } from '../../data';
 import { FaCog } from 'react-icons/fa';
 import { BsSun, BsMoon } from 'react-icons/bs';
 import ThemeColor from './ThemeColor';
 
+const getStorageColor = () => {
+  let color = 'hsl(252, 35%, 51%)';
+  if (localStorage.getItem('color')) {
+    color = localStorage.getItem('color');
+  }
+
+  return color;
+};
+
+const getStorageThemeMode = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+
+  return theme;
+}
+
 const ThemeMode = () => {
   const [showSwitcher, setShowSwitcher] = useState(false);
-  const [color, setColor] = useState('color');
-  const [theme, setTheme] = useState('light-theme');
+  const [color, setColor] = useState(getStorageColor());
+  const [theme, setTheme] = useState(getStorageThemeMode());
 
   const changeColor = (color) => {
     setColor(color);
@@ -22,6 +40,16 @@ const ThemeMode = () => {
     }
   };
 
+  useEffect(() => {
+    document.documentElement.style.setProperty('--first-color', color);
+    localStorage.setItem('color', color);
+  }, [color]);
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <>
       <div className={`${showSwitcher ? 'show-switcher' : ''} style__switcher`}>
@@ -33,7 +61,7 @@ const ThemeMode = () => {
         </div>
 
         <div className="themeMode__toggler" onClick={toggleTheme}>
-          {themeMode === 'light-theme' ? <BsMoon /> : <BsSun />}
+          {theme === 'light-theme' ? <BsMoon /> : <BsSun />}
         </div>
 
         <h3 className="style__switcher-title">테마 설정</h3>
