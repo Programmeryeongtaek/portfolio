d4efae2e7581c051b0aed4f0a59bb7de9fee89f0
YeongTaek <yeongtaek2109@gmail.com>
2023-06-07 10:18:36 +0900

    ScrollUp 구상시작

--- portfolio-final/src/App.jsx
+++ portfolio-final/src/App.jsx
@@ -3,24 +3,24 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Nav from './components/nav/Nav';
 import ThemeMode from './components/themeMode/ThemeMode';
-import Themes from './components/themes/Themes';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Work from './pages/work/Work';
 import Contact from './pages/contact/Contact';
+import ScrollUp from './components/scollup/ScrollUp';
 
 function App() {
   return (
     <BrowserRouter>
       <Nav />
       <ThemeMode />
-      <Themes />
       <Routes>
         <Route index element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='work' element={<Work />} />
         <Route path='contact' element={<Contact />} />
       </Routes>
+      <ScrollUp />
     </BrowserRouter>
   );
 };
