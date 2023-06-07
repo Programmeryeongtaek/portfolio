9870dfa034e612cfd5fe623aaa1fc2049a773fcf
YeongTaek <yeongtaek2109@gmail.com>
2023-06-07 16:45:59 +0900

    Nav 대수정

--- portfolio-final/src/components/nav/Nav.jsx
+++ portfolio-final/src/components/nav/Nav.jsx
@@ -1,97 +1,51 @@
 import React, { useState } from 'react';
 import './nav.css';
-import { AiOutlineAppstore, AiOutlineHome } from 'react-icons/ai';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
-import { HiOutlineDocumentReport } from 'react-icons/hi';
-import { FiSend } from 'react-icons/fi';
-import { FaTimes } from 'react-icons/fa';
+import { AiOutlineAppstore } from 'react-icons/ai';
+import { links } from '../../data';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
   window.addEventListener('scroll', function () {
-    const header = this.document.querySelector('header');
+    const header = this.document.querySelector('nav');
     if (this.scrollY > 80) header.classList.add('scroll-header');
     else header.classList.remove('scroll-header');
   });
 
-  const [Toggle, showMenu] = useState(false);
-  const [activeNav, setActiveNav] = useState('#home');
+  const [Toggle, setToggle] = useState(true);
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <header className="header">
-      <nav className="nav container">
-        <a href="#" className="nav__logo">Yeong</a>
-
-        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
-          <ul className="nav__list">
-            <li className="nav__item">
-              <a 
-                href="#home" 
-                onClick={() => setActiveNav('#home')}
-                className=
-                {
-                  activeNav === '#home'
-                  ? "nav__link active-link"
-                  : "nav__link"
-                }
-              >
-                <AiOutlineHome className='nav__icon' /> Home
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a 
-                href="#about" 
-                onClick={() => setActiveNav('#about')}
-                className=
-                {
-                  activeNav === '#about'
-                  ? "nav__link active-link"
-                  : "nav__link"
-                }
-              >
-                <BsFillPersonLinesFill className='nav__icon' /> About
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a 
-                href="#work" 
-                onClick={() => setActiveNav('#work')}
-                className=
-                {
-                  activeNav === '#work'
-                  ? "nav__link active-link"
-                  : "nav__link"
-                }
-              >
-                <HiOutlineDocumentReport className='nav__icon' /> Work
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a 
-                href="#contact" 
-                onClick={() => setActiveNav('#contact')}
-                className=
-                {
-                  activeNav === '#contact'
-                  ? "nav__link active-link"
-                  : "nav__link"
-                }
-              >
-                <FiSend className='nav__icon' /> Contact
-              </a>
-            </li>
-          </ul>
-
-          <FaTimes className='nav__close' onClick={() => showMenu(!Toggle)} />
-        </div>
-
-        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
-          <AiOutlineAppstore />
-        </div>
-      </nav>
-    </header>
+    <nav className="nav">
+      <div className={`${showMenu ? 'nav__menu show-menu' : 'nav__menu'}`}>
+        <ul className="nav__list">
+          {links.map(({name, icon, path}, index) => {
+            return(
+              <li className="nav__item" key={index}>
+                <NavLink 
+                  to={path} 
+                  className={({isActive}) => isActive ? 'nav__link active-nav' : 'nav__link'} 
+                  onClick={() => {setShowMenu(!showMenu)}}
+                >
+                  {icon}
+                  <h3 className="nav__name">{name}</h3>
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div
+        className={`${showMenu ? 'nav__toggle animate-toggle' : 'nav__toggle'}`}
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="nav__toggler" onClick={() => setToggle(!Toggle)}>
+        <AiOutlineAppstore />
+      </div>
+    </nav>
   );
 };
 
