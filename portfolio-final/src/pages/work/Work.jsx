5068b229ae1409b9a18539104af92e0c571a9ea0
YeongTaek <yeongtaek2109@gmail.com>
2023-06-07 21:03:05 +0900

    Work-portfolio 수정완료

--- portfolio-final/src/pages/work/Work.jsx
+++ portfolio-final/src/pages/work/Work.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import './work.css';
+import { projects } from '../../data';
+import ProjectsItem from './ProjectsItem';
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <main className='work section'>
+      <h2 className="section__title">
+        <span>Work & Study</span>
+      </h2>
+      <section className="work">
+        <div className="portfolio__container container grid">
+          {projects.map((item) => {
+            return <ProjectsItem key={item.id} {...item} />
+          })}
+        </div>
+      </section>
+
+      <section className="study">
+
+      </section>
+    </main>
   );
 };
 
