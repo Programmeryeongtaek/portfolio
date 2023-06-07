08d0fac14771d33b1b5b20d9a9fde07b5f0dc6f2
YeongTaek <yeongtaek2109@gmail.com>
2023-06-07 12:07:50 +0900

    scrollUp 수정

--- portfolio-final/src/components/scollup/ScrollUp.jsx
+++ portfolio-final/src/components/scollup/ScrollUp.jsx
@@ -4,7 +4,7 @@ import { AiOutlineArrowUp } from 'react-icons/ai';
 
 const ScrollUp = () => {
   window.addEventListener('scroll', function () {
-    const scrollUp = document.querySelector('.scrollup');
+    const scrollUp = this.document.querySelector('.scrollup');
     if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
     else scrollUp.classList.remove('show-scroll');
   });
