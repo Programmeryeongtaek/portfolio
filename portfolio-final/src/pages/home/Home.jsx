e2d489d614dda339e28acaf442eef897a95f64b6
YeongTaek <yeongtaek2109@gmail.com>
2023-06-15 10:03:35 +0900

    responsive - home

--- portfolio-final/src/pages/home/Home.jsx
+++ portfolio-final/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Profile from '../../assets/소개.jpg';
 import { Link } from 'react-router-dom';
 import { FaArrowRight } from 'react-icons/fa';
 import HomeSocialLink from './HomeSocialLink';
+import Balancer from 'react-wrap-balancer';
 
 const Home = () => {
   return (
@@ -13,24 +14,29 @@ const Home = () => {
       <div className="home__content">
         <div className="home__data">
           <h1 className="home__title">
-            <span>학습하는 청년, 오영택</span> 
-            <br /> 프론트엔드 개발자
+            <span>학습하는 청년, </span>
+            <span>오영택</span>
+            <br />프론트엔드 개발자
           </h1>
 
           <p className="home__description">
-            나의 성장이 모두의 성장을 촉진하는 사람으로 살아가고 있습니다.
-            <br /> 유능한 프론트엔드 개발자로서 자리잡기 위해 분투하고 있습니다.
-            <br /> 끊임없이 배우고 질문하고 정리하고 있습니다.
+            <Balancer>
+              나의 성장이 모두의 성장을 촉진하는 사람으로 살아가고 있습니다.
+            </Balancer>
+            <Balancer>
+              유능한 프론트엔드 개발자로서 자리잡기 위해 분투하고 있습니다.
+            </Balancer>
+            <Balancer>끊임없이 배우고 질문하고 정리하고 있습니다.</Balancer>
           </p>
 
           <HomeSocialLink />
 
-          <Link to='/about' className='button'>
+          <Link to="/about" className="button">
             자기소개
             <span className="button__icon">
               <FaArrowRight />
             </span>
-          </Link>        
+          </Link>
         </div>
       </div>
 
